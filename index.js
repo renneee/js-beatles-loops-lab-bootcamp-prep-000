1cb8bcc745c74eef2254881dbc11dfffb636bd44
renneee <renpyang@gmail.com>
2017-08-25 12:34:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function theBeatlesPlay (musicians, instruments) {
-  array []
+  array [ ]
 
   for (let i=0, musicians.length = 1; i<1, i++) {
     array.push("${musicians[i]} plays ${instruments[i]}")
   }
   return array
 }
-
