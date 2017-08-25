9466913be1b5b1ae7dc13d435a1d5088f0c564e8
renneee <renpyang@gmail.com>
2017-08-25 12:22:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay (musicians, instruments) {
+
+  var array = [ ]
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
+}
+
+function johnLennonFacts (facts) {
+  while (facts.length > 0) {
+    console.log ("${facts}!!!");
+  }
+}
